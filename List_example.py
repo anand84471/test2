a1209eac332baa25406aa2c0c15b37774122cb41
surabhi-genetics <surabhi.kandaswamy@gmail.com>
2021-09-18 15:46:29 +0100

    Create List_example.py

---
+++ List_example.py
@@ -0,0 +1,21 @@
+saha=[1,2,3,4]
+print(saha)
+print(len(saha))
+print(saha[3])
+print(saha[-2])
+sequences=["ATGCCTGGAAAATTT", "ACCGGGCCCTACGG", "AGGGGCCCTTTTCCCAAA"]
+print (sequences)
+sequences.append("AGCGCGA")
+print(sequences)
+sequences.insert(1,"AGCAGC")
+print(sequences)
+dict_example={"name":"Aaradhya", "rollno":201678, "marks":89}
+print(dict_example["name"])
+print(dict_example["rollno"])
+gc_percentage=93.7
+if gc_percentage<80:
+    #if block
+    print("This is not an ideal seq")
+else:
+    #else block
+    print("This is an ideal seq")
